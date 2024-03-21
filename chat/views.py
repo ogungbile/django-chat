e1da9222c3716f44e49907b4069a3f1fc1614ee7
ogungbile <oluwatobi.ogungbile@yahoo.com>
2024-03-21 13:55:27 +0100

    function send is created and added to view

--- chat/views.py
+++ chat/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from chat.models import Room, Message
+from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
@@ -8,7 +9,7 @@ def home(request):
 def room(request, room):
     username = request.GET.get('username')
     room_details = Room.objects.get(name=room)
-    
+
     return render(request, 'room.html', {
         'username': username,
         'room': room,
@@ -26,4 +27,12 @@ def checkview(request):
         new_room = Room.objects.create(name=room)
         new_room.save()
         return redirect('/'+room+'/?username='+username)
-    
+
+def send(request):
+    message = request.POST['message']
+    username = request.POST['username']
+    room_id = request.POST['room_id']
+
+    new_message = Message.objects.create(value=message, user=username, room=room_id)
+    new_message.save()
+    return HttpResponse('Message sent successfully')
