e9993a56e94efbf87aee3d4ff3210a81fed2d58e
ogungbile <oluwatobi.ogungbile@yahoo.com>
2024-03-21 13:03:04 +0100

    checkview function is added

--- chat/models.py
+++ chat/models.py
@@ -3,9 +3,10 @@ from datetime import datetime
 
 # Create your models here.
 class Room(models.Model):
-    name = models.CharField(max_length 1000)
+    name = models.CharField(max_length=1000)
+
 class Message(models.Model):
-    value = models.CharField(max_length 1000000)
-    date = models.DateTimeField(default datetime.now, blank=True)
+    value = models.CharField(max_length=1000000)
+    date = models.DateTimeField(default=datetime.now, blank=True)
     user = models.CharField(max_length=1000000)
-    room = models.CharField(max_length 1000000)
+    room = models.CharField(max_length=1000000)
